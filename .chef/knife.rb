fcd09a9ddb8156dda716d043446b5b7f5e25b355
Tristan Hume <tris.hume@gmail.com>
2013-05-06 16:54:29 -0400

    Initial commit

---
+++ .chef/knife.rb
@@ -0,0 +1,11 @@
+current_dir = File.dirname(__FILE__)
+log_level                :info
+log_location             STDOUT
+node_name                "trishume"
+client_key               "#{current_dir}/trishume.pem"
+validation_client_name   "trishume-validator"
+validation_key           "#{current_dir}/trishume-validator.pem"
+chef_server_url          "https://api.opscode.com/organizations/trishume"
+cache_type               'BasicFile'
+cache_options( :path => "#{ENV['HOME']}/.chef/checksums" )
+cookbook_path            ["#{current_dir}/../cookbooks"]
